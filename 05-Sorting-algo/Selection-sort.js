f0408614e028b4bddc95c9b5155635953db328c2
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-03-07 07:54:25 +0530

    some example of sorting with selection sort algorithms

--- 05-Sorting-algo/Selection-sort.js
+++ 05-Sorting-algo/Selection-sort.js
@@ -12,21 +12,21 @@ Explanation: After sorting the array is: 1, 2, 3, 4, 5
  */
 
 
-// function Sorting(arr){
-//     let n = arr.length;
+function Sorting(arr){
+    let n = arr.length;
 
-//     for(let i =0;i<=n-1;i++){
-//         let mini = i;
-//         for(let j =i;j<n;j++){
-//             if(arr[j]< arr[mini]){
-//                 mini = j;
-//             }
-//         }
-//         let temp = arr[mini];
-//         arr[mini] = arr[i];
-//         arr[i] = temp;
-//     }
-//     return arr;
-// }
+    for(let i =0;i<=n-1;i++){
+        let mini = i;
+        for(let j =i;j<n;j++){
+            if(arr[j]< arr[mini]){
+                mini = j;
+            }
+        }
+        let temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
 
-// console.log(Sorting([13, 46, 24, 52, 20, 9]))
+console.log(Sorting([13, 46, 24, 52, 20, 9]))
--- 05-Sorting-algo/selectionsort.js
+++ 05-Sorting-algo/selectionsort.js
@@ -1,29 +1,29 @@
-function selectionSort(arr){
-
-    let n = arr.length
-    for(let i =0;i<n-1;i++){
-  let min_idx = findMinIdx(arr,i)
-  if(min_idx !== i){
-    let temp = arr[i];
-    arr[i] = arr[min_idx];
-    arr[min_idx] = temp
+function selectionSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min_idx = findMinIdx(arr, i);
+    if (min_idx !== i) {
+      let temp = arr[i];
+      arr[i] = arr[min_idx];
+      arr[min_idx] = temp;
+    }
   }
-    }
 }
 
+function findMinIdx(arr, i) {
+  let min_idx = i;
 
-function findMinIdx(arr,i){
-    let min_idx = i;
-
-    for(let j = i+1;j<arr.length;j++){
-        if(arr[j]<arr[min_idx]){
-            min_idx = j;
-        }
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[j] < arr[min_idx]) {
+      min_idx = j;
     }
-    return min_idx;
+  }
+  return min_idx;
 }
 
+const arr = [5, 4, 3, 2, 1];
+selectionSort(arr);
+console.log(arr);
 
-const arr = [5,4,3,2,1]
-selectionSort(arr)
-console.log(arr)
+
+
