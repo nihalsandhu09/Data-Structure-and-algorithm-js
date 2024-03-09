64800062bdd177fbede01066a20f7605ab3455c7
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-03-09 20:51:45 +0530

    sorting algo

--- 05-Sorting-algo/InsertionSort.js
+++ 05-Sorting-algo/InsertionSort.js
@@ -15,6 +15,46 @@ function insertionSort(arr){
     }
 }
 
+// const arr = [7,8,9,10,7,8,9,5,9,7]
+// insertionSort(arr)
+// console.log(arr)
+
+
+// function InsertionsortTwo(arr){
+//     let n = arr.length;
+
+//     for(let i=1;i<n;i++){
+//         let element = arr[i];
+//         let j = i-1;
+//         for(;j>=0;j--){
+//             if(arr[j]>element){
+//                 arr[j+1] = arr[j]
+//             }else{
+//                 break;
+//             }
+//         }
+//         arr[j+1] = element;
+//     }
+// }
+
+function InsertioSort(arr){
+    let n = arr.length;
+    for(let i =1;i<n;i++){
+        let element = arr[i];
+        let j = i-1;
+        for(;j>0;j--){
+            if(arr[j]>element){
+                arr[j+1] = arr[j]
+            }else{
+                break;
+            }
+            
+        }
+        arr[j+1] =element;
+    }
+    
+}
+
 const arr = [7,8,9,10,7,8,9,5,9,7]
-insertionSort(arr)
+InsertioSort(arr)
 console.log(arr)
