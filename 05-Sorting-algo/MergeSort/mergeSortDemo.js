48db00a667577a34d25dd44106d1d5475c5c571a
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-03-13 09:34:06 +0530

    mergeSort implimatation on array

--- 05-Sorting-algo/MergeSort/mergeSortDemo.js
+++ 05-Sorting-algo/MergeSort/mergeSortDemo.js
@@ -8,5 +8,29 @@
     }
 
     const middle = Math.floor(arr.length / 2 );
-    
+    const left = arr.slice(0,middle);
+    const right = arr.slice(middle);
+    return merge(mergesortArray(left), mergesortArray(right))
  }
+
+ function merge(left,rigth){
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0
+    
+    while(leftIndex<left.length && rightIndex < rigth.length){
+  if(left[leftIndex] < right[rightIndex] ){
+    result.push(left[leftIndex]);
+    leftIndex++;
+  }else{
+    result.push( rigth[rightIndex])
+    right++;
+  }
+
+    }
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
+}
+
+const arr = [5,3,8,4,2,1,10,7]
+const sortedArr = mergesortArray(arr)
+console.log(sortedArr)
