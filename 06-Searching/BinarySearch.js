467bc8e0721b6856a7fec83efa37265d47b9189f
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-04-03 09:41:23 +0530

    binary search

--- 06-Searching/BinarySearch.js
+++ 06-Searching/BinarySearch.js
@@ -5,9 +5,13 @@ let low = 0, high = n-1;
 while(low<=high){
     let mid = Math.floor((low+high) / 2);
 
-    if(nums[mid] === target) return mid;
-    else if(target > nums[mid]) low = mid+1;
-    else high = mid-1;
+    if(nums[mid] === target){
+        return mid;
+    } 
+    else if(target > nums[mid])
+     {low = mid+1;
+    }
+    else {high = mid-1;}
 }
 return -1;
 }
---
+++ 06-Searching/binarySearchDemo.js
@@ -0,0 +1,31 @@
+const BinarySearch = function(nums,target){
+   
+     let n = nums.length;
+     let high = n-1;
+     let low = 0;
+
+     while(low <= high){
+
+   
+         let mid = Math.floor((low+high)/2)
+          
+         if(nums[mid] === target){
+            return mid;
+         }
+         else if(nums[mid] < target ){
+            low = mid+1;
+         }else{
+            high = mid-1
+         }
+         
+     }
+     return -1;
+     
+
+}
+
+let a = [3, 4, 6, 7, 9, 12, 16, 17];
+let target = 6;
+let ind = BinarySearch(a, target);
+if (ind === -1) console.log("The target is not present.");
+else console.log("The target is at index:", ind);
