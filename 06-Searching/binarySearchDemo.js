83ccacac181ee9f91eb045655c640b8c2fbb96fe
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-04-04 21:10:30 +0530

    nothing cant code some probolems

--- 06-Searching/binarySearchDemo.js
+++ 06-Searching/binarySearchDemo.js
@@ -25,7 +25,7 @@ const BinarySearch = function(nums,target){
 }
 
 let a = [3, 4, 6, 7, 9, 12, 16, 17];
-let target = 6;
+let target = 3;
 let ind = BinarySearch(a, target);
 if (ind === -1) console.log("The target is not present.");
 else console.log("The target is at index:", ind);
