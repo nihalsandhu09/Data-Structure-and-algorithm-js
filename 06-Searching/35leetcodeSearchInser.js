62dfa120b897fa5929cf5c0dc81cc25e0b17882a
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-04-10 12:41:15 +0530

    binary search question

---
+++ 06-Searching/35leetcodeSearchInser.js
@@ -0,0 +1,60 @@
+/**35. Search Insert Position
+Solved
+Easy
+Topics
+Companies
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 104
+    -104 <= nums[i] <= 104
+    nums contains distinct values sorted in ascending order.
+    -104 <= target <= 104
+
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */function lowerBound(arr,x){
+    let n = arr.length;
+    let low = 0, high = n-1;
+    let ans = arr.length;
+
+    while(low <= high){
+        let mid = low + Math.floor((high-low)/2);
+        if(arr[mid]<x){
+            low = mid+1;
+        }else{
+            ans = mid
+            high = mid -1 ;
+        }
+    }
+    return ans;
+}
+var searchInsert = function(nums, target) {
+    return lowerBound(nums,target)
+};
