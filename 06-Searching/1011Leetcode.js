dc7d483800b73b8957c9183cbb18696dbfc6eb96
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-04-16 14:09:37 +0530

    leetcode capacity to ship 1011

--- 06-Searching/1011Leetcode.js
+++ 06-Searching/1011Leetcode.js
@@ -60,30 +60,79 @@ Constraints:
  * @param {number} days
  * @return {number}
  */
+// var shipWithinDays = function(weights, days) {
+//     let left = Math.max(...weights); // minimum capacity
+//       let right = weights.reduce((a, b) => a + b); // maximum capacity
+  
+  
+//       while (left < right) {
+//           let mid = left + Math.floor((right - left) / 2);
+//           let currentDays = 1;
+//           let currentWeight = 0;
+  
+//           for (let i = 0; i < weights.length; i++) {
+//               currentWeight += weights[i];
+//               if (currentWeight > mid) {
+//                   currentDays++;
+//                   currentWeight = weights[i];
+//               }
+//           }
+  
+//           if (currentDays > days) {
+//               left = mid + 1;
+//           } else {
+//               right = mid;
+//           }
+//       }
+  
+//       return left;
+//   }
+
+
+//   
+
+/**
+ * @param {number[]} weights
+ * @param {number} days
+ * @return {number}
+ */
+
+function canShipWithMidWeightWithinDdays(weights, mid, d) {
+    let daysCount = 0;
+    let tw = 0;
+    for(let i = 0; i < weights.length; i++) {
+        if(tw + weights[i] > mid) {
+            daysCount++;
+            tw = 0;
+        }
+
+        tw += weights[i];
+    }
+    daysCount++;
+    return daysCount <= d;
+}
+
 var shipWithinDays = function(weights, days) {
-    let left = Math.max(...weights); // minimum capacity
-      let right = weights.reduce((a, b) => a + b); // maximum capacity
-  
-  
-      while (left < right) {
-          let mid = left + Math.floor((right - left) / 2);
-          let currentDays = 1;
-          let currentWeight = 0;
-  
-          for (let i = 0; i < weights.length; i++) {
-              currentWeight += weights[i];
-              if (currentWeight > mid) {
-                  currentDays++;
-                  currentWeight = weights[i];
-              }
-          }
-  
-          if (currentDays > days) {
-              left = mid + 1;
-          } else {
-              right = mid;
-          }
-      }
-  
-      return left;
-  }
+    let totalWeights = 0;
+    let maxWeight = -1;
+
+    for(let i = 0; i < weights.length ; i++) {
+        totalWeights += weights[i];
+        maxWeight = Math.max(maxWeight, weights[i]);
+    }
+
+    let lo = maxWeight, hi = totalWeights;
+    let ans = hi;
+
+    while(lo <= hi) {
+        let mid = lo + Math.floor((hi - lo) / 2);
+        if(canShipWithMidWeightWithinDdays(weights, mid, days)) {
+            ans = mid;
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
+    return ans;
+};
