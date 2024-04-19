2e42d4fa113f0c68f0ce17a74d0d650349efaa51
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-04-19 17:18:05 +0530

    410 LeetCpde

--- 06-Searching/410-Split_ArrayLeetcode.js
+++ 06-Searching/410-Split_ArrayLeetcode.js
@@ -73,6 +73,39 @@ Constraints:
     console.log(splitArray([1,2,3,4,5], 2)); // Output: 9
     
 
-
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function canSplitInToKSubArraysWithmaxMidSum(nums, mid, k){
+    let subArrayCount  = 0;
+    let totalSum = 0;
+    for(let i = 0; i < nums.length;i++){
+        if(totalSum + nums[i] > mid){
+            subArrayCount++;
+            totalSum = 0;
+        }
+        totalSum += nums[i]
+    }
+      subArrayCount++;
+      return subArrayCount <= k;
+}
+var splitArray = function(nums, k) { 
+let low = Math.max(...nums);
+let high = nums.reduce((acc, cur) => acc + cur, 0);
+let ans = high;
+while(low <= high){
+    let mid = low + Math.floor((high - low) / 2);
+    
+    if(canSplitInToKSubArraysWithmaxMidSum(nums, mid, k)){
+     ans = mid ; 
+     high = mid - 1;
+    }else{
+        low = mid + 1 ;
+    }
+}
+return ans;
+}
 
     
