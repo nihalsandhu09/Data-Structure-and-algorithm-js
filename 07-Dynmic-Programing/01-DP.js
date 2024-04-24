4df4870e36f420c84a73d070bd825edfe3220d82
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-04-24 14:18:46 +0530

    DP

--- 07-Dynmic-Programing/01-DP.js
+++ 07-Dynmic-Programing/01-DP.js
@@ -1,14 +1,11 @@
-function fibonacci(n) {
-    let fib = [];
-    fib[0] = 0;
-    fib[1] = 1;
-    for (let i = 2; i <= n; i++) {
-      fib[i] = fib[i - 1] + fib[i - 2];
-    }
-    return fib[n];
-  }
-  
-  console.log(fibonacci(5)); // Output: 5
-  console.log(fibonacci(10)); // Output: 55
-  console.log(fibonacci(15)); // Output: 610
-  
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function f(n){
+    if(n==0 || n ==1) return n;
+    return f(n-1) + f(n-2);
+ }
+var fib = function(n) {
+ return f(n)
+};
