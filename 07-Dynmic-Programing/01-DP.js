afa8d3f1b4fb6dd22d2f316608d40228b12b3948
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-04-24 16:02:44 +0530

    DP in js Leetcode 501

--- 07-Dynmic-Programing/01-DP.js
+++ 07-Dynmic-Programing/01-DP.js
@@ -2,10 +2,16 @@
  * @param {number} n
  * @return {number}
  */
+let dp;
 function f(n){
-    if(n==0 || n ==1) return n;
-    return f(n-1) + f(n-2);
- }
+   
+   if(n == 0 || n ==1) return n; // base case 
+       if(dp[n] != -1) return dp[n] ; // if value is not -1 we can directly return ans
+       // if the above if casae doesn't execute , it means subproblem has not been solved
+       return dp[n] =  f(n-1) + f(n-2);
+ 
+}
 var fib = function(n) {
- return f(n)
+     dp = Array(100005).fill(-1);
+       return f(n);
 };
