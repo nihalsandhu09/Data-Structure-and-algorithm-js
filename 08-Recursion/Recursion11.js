756431b87291b1c07b731c93ef959febece1655c
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-05-04 14:20:50 +0530

    Recursion palindrome or reverse Array

--- 08-Recursion/Recursion11.js
+++ 08-Recursion/Recursion11.js
@@ -1,7 +1,15 @@
 // check if string is palindrome or Not using Recursion 
 
-function f(i ,s){
-    if(s[i] != s[n-i-1]) return true;
-    if(s.charAt(i))
+function isPalindrome(s, i = 0) {
+    if (i >= s.length / 2) return true;
+    if (s.charAt(i) !== s.charAt(s.length - i - 1)) return false;
+    return isPalindrome(s, i + 1);
 }
 
+function checkPalindrome(s) {
+    return isPalindrome(s) ? "Palindrome" : "Not Palindrome";
+}
+
+// Example string.
+let s = "madam";
+console.log(checkPalindrome(s));
